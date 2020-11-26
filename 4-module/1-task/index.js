d1901260cb27edec2ecf25041a4cc7cf0b6eef0a
Lynxfx <elnurkameow@gmail.com>
2020-11-26 21:13:51 +0600

    4module 1/2/3 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+const list = document.createElement('UL')
+        for (let name of friends) {
+                    list.innerHTML += `<li>${name.firstName} ${name.lastName}</li>`
+                  
+        }
+          return list
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,27 @@
  * @param {Element} table
  */
 function highlight(table) {
+        for (let i = 1; i < table.rows.length; i++) {
+                    let row = table.rows[i].cells
+                for (let cel of row) {
+                        if (cel.hasAttribute('data-available')) {
+                                if (cel.getAttribute('data-available') === 'true') {
+                                                  table.rows[i].classList.add('available')
+                                } else {
+                                                  table.rows[i].classList.add('unavailable')
+                                }
+                        }
+                        if (!row[3].hasAttribute('data-available')) {
+                                        table.rows[i].setAttribute('hidden', true)
+                        }
+                }
+                if (row[2].innerHTML === 'm') {
+                              table.rows[i].classList.add('male')
+                } else {
+                              table.rows[i].classList.add('female')
+                }
+                if (row[1].innerHTML < 18) {
+                              table.rows[i].style.textDecoration = 'line-through'
+                }
+        }
 }
