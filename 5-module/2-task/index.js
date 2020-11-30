2d24d9320d298a7d9b238a9ea29d36f76569de3b
Lynxfx <elnurkameow@gmail.com>
2020-11-30 19:34:44 +0600

    5module 2 task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
-  // ваш код...
+const btn = document.querySelector('.toggle-text-button')
+          const txt =document.querySelector('#text')
+          btn.addEventListener('click', () => txt.toggleAttribute('hidden'))
 }
