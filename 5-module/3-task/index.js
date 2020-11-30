30aa3aef742db8d340262cc074ea6bd7ba241e66
Lynxfx <elnurkameow@gmail.com>
2020-11-30 21:38:01 +0600

    5 module 3task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+const carusel = document.querySelector('.carousel__inner')
+          const caruselWidth = carusel.offsetWidth
+          const rightBtn = document.querySelector('.carousel__arrow_right')
+          const leftBtn = document.querySelector('.carousel__arrow_left')
+          const slide = document.querySelectorAll('.carousel__slide').length - 1
+          let countClick = 0
+
+          leftBtn.style.display = 'none'
+
+        function move(count, btn) {
+                    carusel.style.transform = `translateX(-${caruselWidth*countClick}px)`
+                if(countClick === count) {
+                              btn.style.display = 'none'
+                            
+                } else {
+                              rightBtn.style.display = ''
+                              leftBtn.style.display = ''
+                            
+                }
+                  
+        }
+
+        rightBtn.addEventListener('click', () => {
+                    countClick = ++countClick
+                    move(slide, rightBtn)
+                  
+        })
+
+        leftBtn.addEventListener('click', () => {
+                    countClick = --countClick
+                    move(0, leftBtn)
+                  
+        })
 }
